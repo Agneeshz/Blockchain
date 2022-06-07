8138b75dcd812110ab6ce8ff76ca9cdd21451a68
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-06-07 15:01:34 +0530

    new file

---
+++ app.py
@@ -0,0 +1,7 @@
+import json
+from web3 import Web3
+
+infura_url="https://mainnet.infura.io/v3/1dcd4c1b8977447f9cf7b925ba7a57e4"
+web3=Web3(Web3.HTTPProvider(infura_url))
+
+print(web3.isConnected())
